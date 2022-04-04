1846dca795a515512cb1365bef82febd3fde0ee6
Julio Santos <julioSantos@hotmail.com>
2022-04-04 10:38:21 -0400

    MainViewModel SelectedEAModel and NameSpacesList populated

---
+++ StateMachineCodeGenerator.Tests/ViewModels/MainViewModelTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StateMachineCodeGenerator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateMachineCodeGenerator.Tests.ViewModels
+{
+    [TestClass]
+    public class MainViewModelTests {
+
+        [TestMethod]
+        public void InstantiationTest() {
+            var target = new MainViewModel();
+            Assert.IsNotNull(target);
+        }
+
+        [DataTestMethod]
+        [DataRow("namespace Common", "Common")]
+        [DataRow("namespace StateMachineCodeGenerator.ViewModels", "StateMachineCodeGenerator.ViewModels")]
+        [DataRow(" namespace StateMachineCodeGenerator.ViewModels", "StateMachineCodeGenerator.ViewModels")]
+        [DataRow("namespace StateMachineCodeGenerator.ViewModels  ", "StateMachineCodeGenerator.ViewModels")]
+        [DataRow("namespace StateMachineCodeGenerator.ViewModels  {", "StateMachineCodeGenerator.ViewModels")]
+        [DataRow("  namespace StateMachineCodeGenerator.ViewModels  {", "StateMachineCodeGenerator.ViewModels")]
+        [DataRow("using system;", null)]
+        [DataRow("using system; namespace MyNamespace", "MyNamespace")]
+        public void GetNamespaceValueTest1(string line, string expected) {
+            //var target = new MainViewModel();
+            var actual = MainViewModel.GetNamespaceValue(line);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void HashSetTest() {
+            var set = new HashSet<string>();
+            set.Add("value");
+            set.Add("value");
+            set.Add("value");
+            Assert.AreEqual(1, set.Count);
+        }
+    }
+}
--- StateMachineCodeGenerator.ViewModels/MainViewModel.cs
+++ StateMachineCodeGenerator.ViewModels/MainViewModel.cs
@@ -2,9 +2,15 @@
 using StateMachineMetadata;
 using StateMachineMetadata.Model;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace StateMachineCodeGenerator.ViewModels
@@ -14,23 +20,27 @@ namespace StateMachineCodeGenerator.ViewModels
         public RelayCommand StartParsingCommand => new RelayCommand((o) => StartParsing());
         public RelayCommand OpenFileExplorerCommand => new RelayCommand(OpenFileExplorer);
 
-        #region SourceFilePath
-        //private string _sourceFilePath = @"C:\Users\santosj25\source\repos\JulioCSantos\StateMachineCodeGenerator\StateMachineMetadata\Dependencies\LaserProcessing Model new.xml";
-        private string _sourceFilePath = @"C:\Users\julio\source\repos\JulioCSantos\StateMachineCodeGenerator\StateMachineMetadata new\Dependencies\LaserProcessing Model new.xml";
-        public string SourceFilePath {
-            get => _sourceFilePath;
-            set => SetProperty(ref _sourceFilePath, value);
+        #region EAXMLFilePath
+        public const string EAXMLFilePathLiteral = @"C:\Users\julio\source\repos\JulioCSantos\StateMachineCodeGenerator\StateMachineMetadata new\Dependencies\LaserProcessing Model new.xml";
+        //public const string EAXMLFilePathLiteral = @"C:\Users\santosj25\source\repos\JulioCSantos\StateMachineCodeGenerator\StateMachineMetadata\Dependencies\LaserProcessing Model new.xml";
+        //private string _eaxmlFilePath = @"C:\Users\santosj25\source\repos\JulioCSantos\StateMachineCodeGenerator\StateMachineMetadata\Dependencies\LaserProcessing Model new.xml";
+        private string _eaxmlFilePath;
+        public string EAXMLFilePath {
+            get => _eaxmlFilePath;
+            set => SetProperty(ref _eaxmlFilePath, value);
         }
-        #endregion SourceFilePath
+        #endregion EAXMLFilePath
 
-        #region m_oStateMachineGeneratedCodeFolderPath_textBox
-        //private string _codeTargetFolderPath = @"C:\Users\santosj25\source\repos\JulioCSantos\StateMachineCodeGenerator\TestsSubject";
-        private string _codeTargetFolderPath = @"C:\Users\julio\source\repos\JulioCSantos\StateMachineCodeGenerator\TestsSubject";
-        public string CodeTargetFolderPath {
-            get => _codeTargetFolderPath;
-            set => SetProperty(ref _codeTargetFolderPath, value);
+        #region TargetSolution
+        public const string TargetSolutionLiteral = @"C:\Users\julio\Documents\Visual Studio 2019\Projects\MyCompanies\Corning\TemplateGrid\TemplateGrid.sln";
+        //public const string TargetSolutionLiteral = @"C:\Users\santosj25\source\repos\JulioCSantos\StateMachineCodeGenerator\TestsSubject";
+        //public const string TargetSolutionLiteral = @"C:\Users\julio\source\repos\JulioCSantos\StateMachineCodeGenerator\TestsSubject";
+        private string _targetSolution;
+        public string TargetSolution {
+            get => _targetSolution;
+            set => SetProperty(ref _targetSolution, value);
         }
-        #endregion m_oStateMachineGeneratedCodeFolderPath_textBox
+        #endregion TargetSolution
 
         #region EAModelsList
         private List<StateMachineMetadata.Model.MainModel> _eAModelsList;
@@ -64,28 +74,113 @@ namespace StateMachineCodeGenerator.ViewModels
         }
         #endregion SelectedNameSpace
 
-        public bool IsModelSelectable => EAModelsList.Count > 1;
+        #region CsFiles
+        private List<FileInfo> _csFiles;
+        public List<FileInfo> CsFiles {
+            get => _csFiles;
+            set => SetProperty(ref _csFiles, value);
+        }
+        #endregion CsFiles
+
+        public bool IsModelSelectable => (EAModelsList?.Count ?? 0) > 1;
 
         #region constructor
         public MainViewModel() {
             this.PropertyChanged += MainViewModel_PropertyChanged;
-            RaisePropertyChanged(nameof(SourceFilePath));
+            //RaisePropertyChanged(nameof(EAXMLFilePath));
+            //RaisePropertyChanged(nameof(TargetSolution));
         }
 
         private void MainViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e) {
             switch (e.PropertyName) {
-                case nameof(SourceFilePath):
-                    EAModelsList = Main.GetStateMachineModelFromEAXMLFile(SourceFilePath);
+                case nameof(EAXMLFilePath):
+                    EAModelsList = Main.GetStateMachineModelFromEAXMLFile(EAXMLFilePath);
                     SelectedEAModel = EAModelsList?.FirstOrDefault();
                     break;
                 case nameof(EAModelsList):
                     RaisePropertyChanged(nameof(IsModelSelectable));
                     break;
+                case nameof(TargetSolution):
+                    NamespacesList = GetNameSpaces(TargetSolution).ToList();
+                    break;
+
             }
 
         }
+
+        private readonly ReaderWriterLockSlim _lock = new (LockRecursionPolicy.SupportsRecursion);
+
+
+        private HashSet<string> GetNameSpaces(string targetSolution) {
+            var solutionFileInfo = new FileInfo(targetSolution);
+            if (solutionFileInfo == null) {throw new ArgumentException(nameof(targetSolution));}
+            var targetDir = solutionFileInfo.Directory;
+            if (targetDir == null) { throw new ArgumentException(nameof(targetSolution)); }
+
+            var csFiles = targetDir.EnumerateFiles("*.cs", SearchOption.AllDirectories)
+                .Select(f => new FileInfo(f.FullName)).ToList();
+
+            var namespaces = new HashSet<string>();
+            foreach (FileInfo file in csFiles) {
+                Debug.WriteLine(file.Name);
+                var namespaceValue = GetNamepaceAsync(file).Result;
+                if (string.IsNullOrEmpty(namespaceValue) == false) { namespaces.Add(namespaceValue);}
+            }
+
+            //Parallel.ForEach(csFiles
+            //    , new ParallelOptions { MaxDegreeOfParallelism = 4 }
+            //    , async (f) => {
+            //    var namespaceValues = await GetNamepaceAsync(f);
+            //    namespaceValues.ForEach(v => {
+            //            _lock.EnterWriteLock();
+            //            try { namespaces.Add(v); }
+            //            finally{ if (_lock.IsWriteLockHeld) _lock.ExitWriteLock(); } 
+            //        });
+            //    });
+
+
+            return namespaces;
+        }
         #endregion constructor
 
+        public const string CSharpExtension = ".cs";
+
+        public async Task<string> GetNamepaceAsync(FileInfo csFile) {
+
+            string namespaceResult = null;
+            //var lines = await File.ReadAllLinesAsync(csFile.FullName, CancellationToken.None);
+            var lines = File.ReadAllLines(csFile.FullName);
+            foreach (var line in lines) {
+                namespaceResult = GetNamespaceValue(line);
+                if (string.IsNullOrEmpty(namespaceResult) == false) {
+                    return namespaceResult;
+                }
+            }
+
+            return namespaceResult;
+        }
+
+        //public const string NamespacePatternLiteral = @"(?<=namespace )\b\w+(\.\w+)+\b";
+        public const string NamespacePatternLiteral = @"(?<=namespace )\b\w+(\.\w+)*\b";
+        public static string GetNamespaceValue(string line) {
+            if (string.IsNullOrEmpty(line)) { return null; }
+
+            //var namespacePattern = @"(?<=namespace )\b\w+\b";
+            var match = Regex.Match(line, NamespacePatternLiteral);
+            var asdf = match.Value;
+            return match.Success ? match.Value : null;
+        }
+        //public string GetNamespaceValue(string line) {
+        //    if (string.IsNullOrEmpty(line)) { return null;}
+
+        //    var namespaceSplits = line.Split(NamespaceLiteral, StringSplitOptions.RemoveEmptyEntries);
+        //    if (namespaceSplits.Any() == false || namespaceSplits[0] != NamespaceLiteral) { return null; }
+
+        //    var namespaceStart = namespaceSplits[^1].TrimStart();
+        //    var namespaceValue = namespaceStart.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        //    return namespaceValue[0];
+        //}
 
         //public MainModel GetEAModelsList(string EA_XML_FilePath) {
         //    EAModelsList = StateMachineMetadata.Main.GetStateMachineModelFromEAXMLFile(EA_XML_FilePath);
@@ -94,13 +189,18 @@ namespace StateMachineCodeGenerator.ViewModels
 
         public void OpenFileExplorer(object path) {
 
+            if (string.IsNullOrEmpty(EAXMLFilePath) == false && string.IsNullOrEmpty(TargetSolution)) {
+                TargetSolution = TargetSolutionLiteral;
+            }
+            if (string.IsNullOrEmpty(EAXMLFilePath)) { EAXMLFilePath = EAXMLFilePathLiteral; }
+
         }
 
         public void StartParsing() {
-            //Main.MainEntryPoint(new string[] { SourceFilePath, CodeTargetFolderPath });
+            //Main.MainEntryPoint(new string[] { EAXMLFilePath, TargetSolution });
             //var models = Main.MainModels;
             //var codeGenerator = new StateMachineGenerator();
-            //codeGenerator.Generate(models.FirstOrDefault(), CodeTargetFolderPath);
+            //codeGenerator.Generate(models.FirstOrDefault(), TargetSolution);
         }
 
     }
