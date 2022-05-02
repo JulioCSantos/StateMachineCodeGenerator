dfd49aa3ef10573aa929cae0ee567b37cf915891
Julio Santos <SantosJ25@corning.com>
2022-05-02 19:55:56 -0400

    DirectoryInfoExtensions & DirectoryInfoExtensionsTests created

---
+++ StateMachineCodeGenerator.Common/Extensions/DirectoryInfoExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateMachineCodeGenerator.Common.Extensions
+{
+    public static class DirectoryInfoExtensions
+    {
+        public static IEnumerable<DirectoryInfo> GetSiblings(this DirectoryInfo startFolder) {
+            var parent = startFolder.Parent;
+            var siblings = parent?.EnumerateDirectories();
+            return siblings;
+        }
+
+        public static FileInfo[] GetFiles(this DirectoryInfo searchFolder, string fileNamePattern) {
+            var result = searchFolder.GetFiles(fileNamePattern, searchOption: SearchOption.TopDirectoryOnly);
+
+            return result;
+        }
+
+        public static List<FileInfo> GetFilesInSiblings(this DirectoryInfo startFolder, string fileNamePattern) {
+            var files = new List<FileInfo>();
+            foreach (var dir in startFolder.GetSiblings()) {
+                files.AddRange(dir.GetFiles(fileNamePattern));
+            }
+
+            return files;
+        }
+
+        public static List<FileInfo> FindFirstFilesInAncestors(this DirectoryInfo startFolder, string fileNamePattern, int levelsUp = 3) {
+            if (startFolder == null) { throw new Exception(nameof(startFolder)); }
+            var files = new List<FileInfo>();
+            var parent = startFolder;
+
+            for (int i = 0; i < levelsUp; i++) {
+                parent = parent.Parent;
+                if (parent == null) { throw new Exception(nameof(startFolder)); }
+                files.AddRange(parent.GetFilesInSiblings(fileNamePattern));
+                if (files.Any()) { break; }
+            }
+
+            return files;
+        }
+
+    }
+}
---
+++ StateMachineCodeGenerator.Tests/Common/Extensions/DirectoryInfoExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StateMachineCodeGenerator.Common.Extensions;
+
+namespace StateMachineCodeGenerator.Tests.Common.Extensions
+{
+    [TestClass]
+    public class DirectoryInfoExtensionsTests
+    {
+        [TestMethod]
+        public void GetFilesTest() {
+            var dir = new DirectoryInfo(@"C:\Users\santosj25\source\GenSysTest\MotionSolutions\LLSystemNew");
+            if (dir.Exists == false) { Assert.Inconclusive("directory not found"); }
+
+            var actual = dir.GetFiles("*.sln");
+            Assert.IsTrue(actual.Any());
+        }
+
+        [TestMethod]
+        public void GetFilesInSiblingsTest() {
+            var dir = new DirectoryInfo(@"C:\Users\santosj25\source\GenSysTest\MotionSolutions\LLSystemNew\Modeling");
+            if (dir.Exists == false) { Assert.Inconclusive("directory not found"); }
+
+            var actual = dir.GetFilesInSiblings("*.xml");
+            Assert.IsTrue(actual.Any());
+        }
+
+        [TestMethod]
+        public void FindFirstFilesInAncestorsTest() {
+            var dir = new DirectoryInfo(@"C:\Users\santosj25\source\GenSysTest\MotionSolutions\LLSystemNew\Modeling");
+            if (dir.Exists == false) { Assert.Inconclusive("directory not found"); }
+
+            var actual = dir.FindFirstFilesInAncestors("*.sln");
+            Assert.IsTrue(actual.Any());
+        }
+    }
+}
